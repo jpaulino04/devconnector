f52ff98fd5bb3418128cf08131c297859cf69e5c
Jose Paulino <jpaulino04@msn.com>
2019-05-31 13:22:00 -0400

    added github get repos api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const {check, validationResult} = require('express-validator/check');
 
@@ -307,6 +309,38 @@ router.delete('/education/:edu_id', auth, async(req, res) => {
     }
 
 
+})
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+// @route GET api/profile/github/:username
+// @desc  GET user repos from Github
+// @access Public
+
+router.get('/github/:username', (req, res) => {
+    
+    try {
+
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubClientSecret')}`,
+            method: 'GET',
+            headers: {'user-agent': 'node.js'}
+        }
+
+        request(options, (error, response, body) => {
+            if(error) console.log(error);
+
+            if(response.statusCode !== 200){
+                return res.status(404).json({msg: "No Github Profile found"})
+            }
+
+            res.json(JSON.parse(body));
+        })
+        
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Errror')
+    }
 })
 
 
