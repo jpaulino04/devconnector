92f3275694ec8c69a27861def9f916bc6584e8ae
Jose Paulino <jpaulino04@msn.com>
2019-06-06 07:27:38 -0400

    added POST schema

---
+++ models/Post.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+//Note there is an avatar and a name for the user also here
+//Reason is, we might want to keep the post after deleting the user
+const postSchema = new mongoose.Schema({
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: string
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user:{
+                type: Schema.Types.ObjecId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: string
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+
+const Post = mongoose.model('Post', postSchema)
+module.exports = Post;
