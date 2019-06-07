ded3220ca17d2b54faceaf971f8131a5521bb2f8
Jose Paulino <jpaulino04@msn.com>
2019-06-07 09:27:08 -0400

    create new poset in post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResults} = require('express-validator/check');
 
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('Hello from posts!')
+// @route POST /api/posts
+// @desc  Create a post
+//@access Private
+router.get('/', [auth,
+    check('text', 'Text is required').not().isEmpty()
+],
+async (req, res) => {
+    const errors = validationResults(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const user = await User.findById(req.user.id).select('-password')
+
+    try {
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("Server Error")
+    }
 })
 
 
