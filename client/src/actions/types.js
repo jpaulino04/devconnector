6043d6ab4cf6d700bb740ba3777fa7728d615ff3
Jose Paulino <jpaulino04@msn.com>
2019-07-12 14:54:38 -0400

    Started the auth reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,3 +2,5 @@
 //Notice you will import these in different files.
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
