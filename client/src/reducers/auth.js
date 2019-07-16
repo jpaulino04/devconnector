16d107c8cac8d3ef905567e078308121fe508c97
Jose Paulino <jpaulino04@msn.com>
2019-07-16 12:23:18 -0400

    working on auth, added the initial state

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,1 +1,12 @@
 import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+
+const initialState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null
+}
+
+export default function(state = initialState, action){
+
+}
